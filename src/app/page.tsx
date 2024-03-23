aeec48770131c81a838836689955c370f02064bc
swoichha adhikari <swoichha@swoichhas-MacBook-Pro.local>
2024-03-23 18:39:14 +0545

    added avatar, badge, button on the card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,9 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
+import { Badge } from "@/components/ui/badge";
 
 interface Recipe {
   title: string;
@@ -31,6 +34,10 @@ export default async function Home() {
         {recipes.map((recipe) => (
           <Card key={recipe.id} className="flex flex-col justify-between">
             <CardHeader className="flex-row gap-4 items-center">
+              <Avatar>
+                <AvatarImage src={`/img/${recipe.image}`} alt="receipe_img" />
+                <AvatarFallback>{recipe.title.slice(0, 2)}</AvatarFallback>
+              </Avatar>
               <div>
                 <CardTitle>{recipe.title}</CardTitle>
                 <CardDescription>{recipe.time} mins to cook.</CardDescription>
@@ -40,8 +47,8 @@ export default async function Home() {
               <p>{recipe.description}</p>
             </CardContent>
             <CardFooter className="flex justify-between">
-              <button>View Recipe</button>
-              {recipe.vegan && <p>Vegan!</p>}
+              <Button>View Recipe</Button>
+              {recipe.vegan && <Badge variant={"secondary"}>Vegan!</Badge>}
             </CardFooter>
           </Card>
         ))}
